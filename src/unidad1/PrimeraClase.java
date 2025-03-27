49dd252c4f1357e833c2f4ab3b41663992f6f5eb
leotorresval92 <leotorresval92@gmail.com>
2025-03-27 10:29:36 -0500

    Cambios en primera clase

--- src/unidad1/PrimeraClase.java
+++ src/unidad1/PrimeraClase.java
@@ -3,6 +3,8 @@ package unidad1;
 public class PrimeraClase {
     public static void main(String [] args){
         System.out.println("Hola mundo");
+        System.out.println("Pruebas de repositorio");
+        System.out.println("Nuevo cambio");
     }
     
 }
